25f94c8c8ec0298c11f964371cdc2f47d67b1b4b
vcc1432 <valerie.gersen@gmail.com>
2020-10-08 18:50:26 +0200

    Http Error model, query swapi with querystring

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const HttpError = require('./models/http-error');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,14 +13,27 @@ app.use(express.static('public'));
 app.get('/search', async (req, res, next) => {
   let query = req.query.q;
   let response; 
+
   try {
     response = await axios.get(`https://swapi.dev/api/films/?search=${query}`);
-    console.log('data: ', response.data);
   } catch (err) {
-    console.log(`Got error: ${e}`);
+    const error = new HttpError(
+      'Fetching places failed, please try again later', 
+      500
+    );
+    return next(error);
   }
 
-  res.json(response.data);
+  const data = response.data;
+  if (!data.results || data.results.length === 0) {
+    const error = new HttpError(
+      'Could not find a film for the provided query.', 
+      404
+    );
+    return next(error);
+  }
+
+  res.json(data);
 });
 
 app.listen(3000, () => {
